fb6142a7d2382cd2c525809a954e34ace0777809
dragan002 <draganvujic29@gmail.com>
2024-05-14 18:28:40 +0200

    Starting

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::resource('\notes', NoteController::class)->middleware(['auth']);
+require __DIR__.'/auth.php';
